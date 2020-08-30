87add8aeae2595b5b0b4f9db704bef326642e8ba
Sravan Balaji <balajsra@umich.edu>
2020-08-30 16:31:14 -0400

    dwm & BobTheFish Configuration Updates
    - Add ru_gaps, centeredmaster, gridmode, cyclelayouts, and restartsig patches to dwm
    - Enable git stuff, newline cursor, process, user, and hostname for BobTheFish theme

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,8 +10,19 @@ export TERM=xterm-256color
 export XDG_CONFIG_HOME=$HOME/.config
 
 # bobthefish Configuration
+set -g theme_display_git yes
+set -g theme_display_git_dirty yes
+set -g theme_display_git_untracked yes
+set -g theme_display_git_ahead_verbose yes
+set -g theme_display_git_dirty_verbose yes
+set -g theme_display_git_stashed_verbose yes
+set -g theme_display_git_master_branch yes
 set -g theme_nerd_fonts yes
 set -g theme_display_user yes
 set -g fish_prompt_pwd_dir_length 0
 set -g theme_color_scheme dracula
 set -g theme_newline_cursor yes
+set -g theme_nerd_fonts yes
+set -g theme_title_display_process yes
+set -g theme_title_display_user yes
+set -g theme_display_hostname yes

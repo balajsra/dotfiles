d984a5ef98af8f868a14e8ded7862b668175850e
Sravan Balaji <balajsra@umich.edu>
2020-12-11 12:39:45 -0500

    Autostart startx on login
    - Autostart startx on login in fish shell

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,12 @@
+########################
+# AUTOSTART X AT LOGIN #
+########################
+if status --is-login
+    if test -z "$DISPLAY" -a $XDG_VTNR = 1
+        exec startx -- -keeptty
+    end
+end
+
 ############
 # GREETING #
 ############

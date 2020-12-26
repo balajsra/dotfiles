6558dac4e4ce15cc625987ff8fb3916877b5fa3f
Sravan Balaji <balajsra@umich.edu>
2020-12-26 15:12:59 -0500

    Remove Fish Shell Autostart X
    - Switched back to using lightdm (without light-locker)
    - Remove autostart x commands in fish shell

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,12 +1,3 @@
-########################
-# AUTOSTART X AT LOGIN #
-########################
-if status --is-login
-    if test -z "$DISPLAY" -a $XDG_VTNR = 1
-        exec startx -- -keeptty
-    end
-end
-
 ############
 # GREETING #
 ############

26e5c160fd38bf80a5b6a6b8556e025770547d12
Sravan Balaji <balajsra@umich.edu>
2022-05-21 06:40:53 -0400

    Update External HDD Rsync Script
    - Reformatted external HDD as exFAT due to NTFS permission issues
    - Change `external_hdd_path` to "/mnt/Seagate 5TB"
    - Add Pictures directory to backup
    - Add Lutris system settings and configuration to backup
    - Add Dolphin, PCSX2, RetroArch, and RPCS3 system settings to backup

--- .scripts/rsync_laptop_to_external_hdd.sh
+++ .scripts/rsync_laptop_to_external_hdd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Define base paths
-external_hdd_path="/mnt/5TB External HDD"
+external_hdd_path="/mnt/Seagate 5TB"
 backup_path="$external_hdd_path/Backups"
 home_path="/home/sravan"
 
@@ -10,20 +10,34 @@ declare -a source_dirs=(
     "$home_path/Documents/"
     "$home_path/ISOs/"
     "$home_path/Music/"
+    "$home_path/Pictures/"
     "$home_path/Videos/"
     "$home_path/Games/PolyMC/"
     "$home_path/Games/Retro/"
     "$home_path/Games/Steam/steamapps/common/"
+    "$home_path/Games/Lutris/"
+    "$home_path/.config/lutris/"
+    "$home_path/.config/dolphin-emu/"
+    "$home_path/.config/PCSX2/"
+    "$home_path/.config/retroarch/"
+    "$home_path/.config/rpcs3/"
 )
 declare -a target_dirs=(
     "$backup_path/Calibre_Library/"
     "$backup_path/Documents/"
     "$backup_path/ISOs/"
     "$backup_path/Music/"
+    "$backup_path/Pictures/"
     "$backup_path/Videos/"
     "$backup_path/Games/PolyMC/"
     "$backup_path/Games/Retro/"
     "$backup_path/Games/Steam/"
+    "$backup_path/Games/Lutris/"
+    "$backup_path/Config/Lutris/"
+    "$backup_path/Config/Dolphin-Emu/"
+    "$backup_path/Config/PCSX2/"
+    "$backup_path/Config/RetroArch/"
+    "$backup_path/Config/RPCS3/"
 )
 
 # Loop through arrays

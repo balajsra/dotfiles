a47eeb536e9c7958ee8407359685bb45072e5464
Sravan Balaji <balajsra@umich.edu>
2022-07-07 05:56:24 -0400

    Update Monitor Setup Script
    - Replace HDMI output 2K@144Hz with DisplayPort output 2K@165Hz

--- .scripts/monitor_setup.sh
+++ .scripts/monitor_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 xrandr \
     --output DP-0 --primary --mode 2560x1440 --pos 0x0 --rate 164.96 \
-    --output HDMI-0 --mode 2560x1440 --pos 2560x0 --rate 143.93 \
+    --output DP-2 --mode 2560x1440 --pos 2560x0 --rate 164.96 \
     --output eDP-1-1 --mode 1920x1080 --pos 5120x1440 --rate 144.00

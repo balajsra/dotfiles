7e270ed2def04d6591d2a8cc17dcab023a5a154c
Sravan Balaji <balajsra@umich.edu>
2022-08-11 16:25:49 -0400

    Update Submodules & HDD Rsync Scrip
    - Update doom emacs, doom eamcs config, dwm, and
      dracula-gtk submodules
    - Update `rsync_laptop_to_external_hdd.sh` to use
      new path to HDD and re-order the folders that
      get synced

--- .scripts/rsync_laptop_to_external_hdd.sh
+++ .scripts/rsync_laptop_to_external_hdd.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 # Define base paths
-external_hdd_path="/run/media/sravan/Seagate 5TB"
+external_hdd_path="/run/media/sravan/5TB_HDD"
 backup_path="$external_hdd_path/Backups"
 home_path="/home/sravan"
 
 # Define arrays
 declare -a source_dirs=(
+    "$home_path/.config/lutris/"
+    "$home_path/.config/dolphin-emu/"
+    "$home_path/.config/retroarch/"
+    "$home_path/.config/rpcs3/"
+    "$home_path/.config/PCSX2/"
+    "$home_path/.cache/lutris/"
     "$home_path/Calibre_Library/"
     "$home_path/ISOs/"
     "$home_path/Manga/"
@@ -13,14 +19,14 @@ declare -a source_dirs=(
     "$home_path/Pictures/"
     "$home_path/Videos/"
     "$home_path/Games/"
-    "$home_path/.config/lutris/"
-    "$home_path/.config/dolphin-emu/"
-    "$home_path/.config/retroarch/"
-    "$home_path/.config/rpcs3/"
-    "$home_path/.config/PCSX2/"
-    "$home_path/.cache/lutris/"
 )
 declare -a target_dirs=(
+    "$backup_path/.config/lutris/"
+    "$backup_path/.config/dolphin-emu/"
+    "$backup_path/.config/retroarch/"
+    "$backup_path/.config/rpcs3/"
+    "$backup_path/.config/PCSX2/"
+    "$backup_path/.cache/lutris/"
     "$backup_path/Calibre_Library/"
     "$backup_path/ISOs/"
     "$backup_path/Manga/"
@@ -28,12 +34,6 @@ declare -a target_dirs=(
     "$backup_path/Pictures/"
     "$backup_path/Videos/"
     "$backup_path/Games/"
-    "$backup_path/.config/lutris/"
-    "$backup_path/.config/dolphin-emu/"
-    "$backup_path/.config/retroarch/"
-    "$backup_path/.config/rpcs3/"
-    "$backup_path/.config/PCSX2/"
-    "$backup_path/.cache/lutris/"
 )
 
 # Loop through arrays

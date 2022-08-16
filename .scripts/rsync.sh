ab0b36e06eb6c03e6cd2764e7c1e2b5702916ab3
Sravan Balaji <balajsra@umich.edu>
2022-08-16 16:39:48 -0400

    Snap Sync Systemd Service
    - Add service and timer for snap-sync
    - Remove rsync scripts for phone (covered by syncthing)
      and external hdd (covered by snap-sync)
    - Remove options for laptop and phone rsync scripts
      in rsync rofi menu
    - Re-organize some sections of README.org

--- .scripts/rsync.sh
+++ .scripts/rsync.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 help_menu() {
     echo "Main menu to launch run rsync scripts. Use only one argument at a time."
-    # echo "  - Play / Pause:  playerctl.sh --play-pause"
-    # echo "  - Next:          playerctl.sh --next"
-    # echo "  - Previous:      playerctl.sh --prev"
-    # echo "  - Change Player: playerctl.sh --change"
-    # echo "  - Rofi Menu:     playerctl.sh --rofi"
-    # echo "  - Help:          playerctl.sh --help OR playerctl.sh -h"
 }
 
 rofi_menu() {
     declare -a options=(
         "   - rsync-laptop-to-cloud"
-        "   - rsync-laptop-to-external-hdd"
-        "   - rsync-laptop-to-phone"
         " Back - back"
         " Quit - quit"
     )
@@ -39,12 +31,6 @@ main() {
             ;;
         --rsync-laptop-to-cloud)
             kitty --hold /home/sravan/.scripts/rsync_laptop_to_cloud.sh
-            ;;
-        --rsync-laptop-to-external-hdd)
-            kitty --hold /home/sravan/.scripts/rsync_laptop_to_external_hdd.sh
-            ;;
-        --rsync-laptop-to-phone)
-            kitty --hold /home/sravan/.scripts/rsync_laptop_to_phone.sh
             ;;
         --rofi)
             rofi_menu

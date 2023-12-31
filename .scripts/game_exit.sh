8dba18c23f31d2a4eb678a6f42cbd90d6c11cd8d
Sravan Balaji <balajsra@umich.edu>
2023-12-30 23:22:30 -0500

    Fix Picom Issue with Game Scripts
    - Previously, picom would not turn back on after exiting a game
    - Resolved this by running all game launch / exit script commands
      in the background so they persist even after the script itself
      exits

--- .scripts/game_exit.sh
+++ .scripts/game_exit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-/home/sravan/.scripts/picom.sh --on
-/home/sravan/.scripts/deadd.sh --unpause
-/home/sravan/.scripts/cpu-gpu.sh --cpu-profile-balanced
-/home/sravan/.scripts/cpu-gpu.sh --gpu-profile-adaptive
-/usr/bin/xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/presentation-mode -s false
+/home/sravan/.scripts/picom.sh --on &
+/home/sravan/.scripts/deadd.sh --unpause &
+/home/sravan/.scripts/cpu-gpu.sh --cpu-profile-balanced &
+/home/sravan/.scripts/cpu-gpu.sh --gpu-profile-adaptive &
+/usr/bin/xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/presentation-mode -s false &
--- .scripts/game_launch.sh
+++ .scripts/game_launch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-/home/sravan/.scripts/picom.sh --off
-/home/sravan/.scripts/cpu-gpu.sh --cpu-profile-performance
-/home/sravan/.scripts/cpu-gpu.sh --gpu-profile-performance
-/home/sravan/.scripts/deadd.sh --pause
-/usr/bin/xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/presentation-mode -s true
+/home/sravan/.scripts/picom.sh --off &
+/home/sravan/.scripts/cpu-gpu.sh --cpu-profile-performance &
+/home/sravan/.scripts/cpu-gpu.sh --gpu-profile-performance &
+/home/sravan/.scripts/deadd.sh --pause &
+/usr/bin/xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/presentation-mode -s true &

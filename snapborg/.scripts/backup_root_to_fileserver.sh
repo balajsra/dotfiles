15edcc4cab56c641499fc1431564adc47b4c3854
Sravan Balaji <balajsra@umich.edu>
2024-06-11 18:39:06 -0400

    Snapborg Path Updates
    - Change to HDD repo path to /mnt/5TB_HDD
    - Remove backup scripts since it is handled by systemd services

--- snapborg/.scripts/backup_root_to_fileserver.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-sudo snapborg --cfg /home/sravan/.config/snapborg/root_fileserver_config.yaml --bind-mount backup
--- snapborg/.scripts/backup_root_to_hdd.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-sudo snapborg --cfg /home/sravan/.config/snapborg/root_hdd_config.yaml --bind-mount backup

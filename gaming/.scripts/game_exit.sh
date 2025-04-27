b130b973ce9e62263160485638abc457a8837410
Sravan Balaji <balajsra@umich.edu>
2025-04-27 16:16:27 -0400

    Disable animations/blur/etc. when gaming on Hyprland
    - Add hyprctl command to disable animations/blur/etc. when
      launching a game on Hyprland
    - Add hyprctl command to reload config when exiting
      a game on Hyprland
    - Update hyprland submodule

--- gaming/.scripts/game_exit.sh
+++ gaming/.scripts/game_exit.sh
@@ -4,6 +4,10 @@ if [ $XDG_SESSION_DESKTOP == "dwm" ]; then
     /usr/bin/xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/presentation-mode -s false &
 fi
 
+if [ $XDG_SESSION_DESKTOP == "Hyprland" ]; then
+    hyprctl reload &
+fi
+
 $HOME/.scripts/dunst.sh --unpause &
 $HOME/.scripts/cpu-gpu.sh --cpu-profile-balanced &
 $HOME/.scripts/cpu-gpu.sh --gpu-profile-adaptive &
--- gaming/.scripts/game_launch.sh
+++ gaming/.scripts/game_launch.sh
@@ -4,6 +4,17 @@ if [ $XDG_SESSION_DESKTOP == "dwm" ]; then
     /usr/bin/xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/presentation-mode -s true &
 fi
 
+if [ $XDG_SESSION_DESKTOP == "Hyprland" ]; then
+    hyprctl --batch "\
+        keyword animations:enabled 0;\
+        keyword decoration:shadow:enabled 0;\
+        keyword decoration:blur:enabled 0;\
+        keyword general:gaps_in 0;\
+        keyword general:gaps_out 0;\
+        keyword general:border_size 1;\
+        keyword decoration:rounding 0" &
+fi
+
 $HOME/.scripts/cpu-gpu.sh --cpu-profile-performance &
 $HOME/.scripts/cpu-gpu.sh --gpu-profile-performance &
 $HOME/.scripts/dunst.sh --pause &
